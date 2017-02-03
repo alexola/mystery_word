1de4e8ff310dac30cb83aa81fd5e398175833a28
alexola <alexola.22@gmail.com>
2017-02-03 09:17:56 +0100

    final

--- js/main.js
+++ js/main.js
@@ -1,73 +1,109 @@
-
 // get the random word of the array
 
-var rndWords = ["PONCHE","COOPERACHA", "TEQUILA","XOLOITZCUINTLE","NOPAL","VOCHO", "TORTA","MOLE","CATRINA","TLACUACHE"];
+var rndWords = ["PONCHE", "COOPERACHA", "TEQUILA", "XOLOITZCUINTLE", "NOPAL", "VOCHO", "TORTA", "MOLE", "CATRINA", "TLACUACHE"];
 
-// test image display if it equals the rndWords value
-// function image(thisImg) {
-//     var img = doc0ument.createElement("img");
-//     img.src = "img/"+thisImg+".jpg";
-//     document.getElementById('imageDiv').appendChild(img);
-// }
-// if (a == 'tick') {
-//     image('tick.gif');
-// } else {
-//     image('cross.gif');
-// }
+var random;
 
+function startGame(){
+
+      random = rndWords[Math.floor(Math.random() * rndWords.length)];
+      var cut = random.split("");
+      //Gets random words to image display and remove to get new (works)
+      $('#foto').remove();
+      var image = "./img/" + random + ".jpg";
+      $(".container").after($('<img id="foto">').attr("src", image));
+
+      console.log(cut);
+      $('#hidden-word').empty('letters');
+      for (var i = 0; i < cut.length; i++)
+          $('#hidden-word').append("<p class='letters'>" + cut[i] + "</p>");
+      $("p").hide();
+}
 
 
 $("#randomWord").on('click', function() {
-var random = rndWords[Math.floor(Math.random() * rndWords.length)];
-var cut = random.split("");
-//Gets random words to image display
-var image = "./img/" + random + ".jpg";
-$(".container").after($('<img id="foto">').attr("src", image));
-$('<img id="foto">').empty("src", image);
-// aqui poner el empty letters
-//
-console.log(cut);
-  $('#hidden-word').empty('letters');
-for (var i = 0; i < cut.length; i++)
-  $('#hidden-word').append("<p id='letters'>"+ cut[i]+"</p>");
-  $("p").hide();
+startGame();
 });
 
-$("#show").click(function(){
-        alert("No manches!");
-        $("p#letters").show();
+
+$("#show").click(function() {
+    alert("No manches!,perdiste!");
+    $("p.letters").show();
+    counter = 6;
 
 
 });
-//Get to work the keypress to compare values with the random word (fix it)
-// works getting words missing to compare with the random word and if the word repeats some words to use only once no more
-$(document).keypress(function(event){
-var keyPressed= event.key;
-var keyPressedCap = keyPressed.toUpperCase();
-// var found = $.inArray(keyPressedCap, rndWords) > -1;
-// console.log(found);
-// return false;
+//Get to work the keypress to compare values with the random word (work in progress)
+// need to make it align so it doesnt gets crowded
+var counter = 6 ;
 
-if (keyPressedCap === $('#letters')){
-   show.$("p#letters").show();
-  console.log("works");
-}
-console.log(keyPressedCap);
+$(document).keypress(function(event) {
+    var keyPressed = event.key;
+    var keyPressedCap = keyPressed.toUpperCase();
+    var exist = false;
 
-// var look = rndWords(keypress);
-// console.log(look);
+    $('.letters').each(function(index, letter, array) {
+        if (keyPressedCap === $(letter).text()) {
+            $(letter).show();
+            $(letter).attr("data-visible", "true");
+            console.log(keyPressedCap);
+            exist = true;
+        }
+    });
+    // when total value of the random word is fill with correct keys press throws
+    if(random.length === $("[data-visible='true']").length){
+      $("#loser").append("<div class='loser'><img src='img/pinata.jpg' class='img-responsive pinata-img'><h2>YOU WON !!!</h2><br><button  id='refresh' class ='btn btn-info btn-lg'>PLAY AGAIN</button></div>");
+      alert ("¡¡¡ GANASTE !!!");
+      $("#refresh").click(function(){
+        $(".loser").remove();
+        startGame();
+        counter = 6;
+      });
+
+    }
+
+
+    if (exist) {
+        console.log("guay");
+        console.log(counter);
+        counter +=1;
+        $('span').text(counter);
+        exist = false;
+    }
+    else {
+      console.log("no guay");
+      counter -=1;
+      $('span').text(counter);
+      if(counter === 0){
+        alert("you loose");
+
+
+        counter = 6;
+      }
+
+    }
+
+
+
+
+
 });
+// test button to go next works doesnt reset score
 
-  // console.log(keyPressedCap);
-  // var found = $.inArray(keyPressedCap, wordSplitted) > -1;
-  // console.log(found);
+$("#randomWord1").on('click', function() {
+startGame();
+
+});
 
 
 // will show the number of keys pressed (working)
 i = 0;
-$(document).ready(function(){
-    $(document).keypress(function(){
-        $("span").text(i += 1);
-    });
+$(document).ready(function() {
+    // $(document).keypress(function() {
+    //     $("span").text(i += 1);
+    //     if (i === false) {
+    //         alert("you dont know spanish");
+    //     }
+    // });
 });
 //
